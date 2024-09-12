f9da3f75313a5ec0ff8141b269df1e008711cb16
Kirby Phillips <kphillips.za@gmail.com>
2024-09-12 12:05:51 +0200

    Update Objective 3_.sql

--- Objective 3_.sql
+++ Objective 3_.sql
@@ -32,6 +32,8 @@ ORDER BY num_vehicles DESC;
 
 -- 3. Do the types of vehicles stolen in the three most dense regions differ from the three least dense regions?
 
+-- Determine the 3 most and least dense regions:
+
 SELECT l.region, COUNT(sv.vehicle_id) AS num_vehicles,
 	l.population, l.density
 FROM stolen_vehicles sv LEFT JOIN locations l
@@ -39,10 +41,7 @@ ON sv.location_id = l.location_id
 GROUP BY l.region, l.population, l.density
 ORDER BY l.density DESC;
 
-SELECT sv.vehicle_type, COUNT(sv.vehicle_id) AS num_vehicles
-FROM stolen_vehicles sv LEFT JOIN locations l
-ON sv.location_id = l.location_id
-GROUP BY sv.vehicle_type;
+-- Identify types of vehicles stolen in these regions:
 
 SELECT sv.vehicle_type, COUNT(sv.vehicle_id) AS num_vehicles
 FROM stolen_vehicles sv LEFT JOIN locations l
@@ -57,6 +56,8 @@ ON sv.location_id = l.location_id
 WHERE l.region IN ('Otago', 'Gisbourne', 'Southland')
 GROUP BY sv.vehicle_type
 ORDER BY num_vehicles DESC;
+
+-- Create a table to see the results of these regions stacked from high to low, using a UNION:
 
 (SELECT sv.vehicle_type, COUNT(sv.vehicle_id) AS num_vehicles
 FROM stolen_vehicles sv LEFT JOIN locations l
@@ -73,13 +74,16 @@ ON sv.location_id = l.location_id
 WHERE l.region IN ('Otago', 'Gisbourne', 'Southland')
 GROUP BY sv.vehicle_type);
 
+-- Distinguish between these areas by adding new columns such as 'High Density' and 'Low Density':
+
 (SELECT 'High Density' AS density, sv.vehicle_type, COUNT(sv.vehicle_id) AS num_vehicles
 FROM stolen_vehicles sv
 LEFT JOIN locations l
 ON sv.location_id = l.location_id
 WHERE l.region IN ('Auckland', 'Nelson', 'Wellington')
 GROUP BY sv.vehicle_type
-ORDER BY num_vehicles DESC)
+ORDER BY num_vehicles DESC
+LIMIT 5)
 
 UNION
 
@@ -89,29 +93,5 @@ LEFT JOIN locations l
 ON sv.location_id = l.location_id
 WHERE l.region IN ('Otago', 'Gisborne', 'Southland')
 GROUP BY sv.vehicle_type
-ORDER BY num_vehicles DESC);
-
-(SELECT 'High Density' AS density, sv.vehicle_type, COUNT(sv.vehicle_id) AS num_vehicles
-FROM stolen_vehicles sv
-LEFT JOIN locations l
-ON sv.location_id = l.location_id
-WHERE l.region IN ('Auckland', 'Nelson', 'Wellington')
-GROUP BY sv.vehicle_type
 ORDER BY num_vehicles DESC
-LIMIT 5)
-
-UNION ALL
-
-(SELECT 'Low Density' AS density, sv.vehicle_type, COUNT(sv.vehicle_id) AS num_vehicles
-FROM stolen_vehicles sv
-LEFT JOIN locations l
-ON sv.location_id = l.location_id
-WHERE l.region IN ('Otago', 'Gisborne', 'Southland')
-GROUP BY sv.vehicle_type
-ORDER BY num_vehicles DESC
-LIMIT 5)
-ORDER BY num_vehicles DESC;
-
--- 4. Data Visualization: Create a scatter plot.
-
--- 5. Data Visualization: Create a map.
+LIMIT 5);
